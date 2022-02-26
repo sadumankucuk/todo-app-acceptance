aa529f201f551137402b2584d1c61ac6bc6a5468
sadumankucuk <sadumankucuk@gmail.com>
2022-02-26 23:43:42 +0300

    write When test

--- features/steps/todo-app.js
+++ features/steps/todo-app.js
@@ -2,6 +2,8 @@ const {Given, When, Then} = require("cucumber");
 const openUrl = require("../support/action/openUrl");
 const checkElementExists = require("../support/check/checkElementExists");
 const checkElementValue = require("../support/check/checkElementValue");
+const sendKeys = require("../support/action/sendKeys");
+const clickElement = require("../support/action/clickElement");
 
 Given(/^Empty ToDo list$/, async function () {
     await openUrl.call(this, "/")
@@ -10,8 +12,9 @@ Given(/^Empty ToDo list$/, async function () {
     await checkElementExists.call(this, "#add-todo", false)
 });
 
-When(/^User write "([^"]*)" to text box and click to add button$/, function () {
-
+When(/^User write "([^"]*)" to text box and click to add button$/, async function (newTodo) {
+    await sendKeys.call(this, "#todo-input", newTodo)
+    await clickElement.call(this, "#add-todo")
 });
 
 Then(/^User should see "([^"]*)" item in ToDo list$/, function () {
